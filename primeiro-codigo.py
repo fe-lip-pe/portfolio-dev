de2a9f57325a63b7ab7d2781a42c46dd7ef1c23f
Felipe Roosevelt Duarte <feliperoosevelt551@gmail.com>
2026-05-05 14:30:05 -0300

    Add basic calculator functionality in primeiro-codigo.py
    Implement a basic calculator program in Portugol.

--- primeiro-codigo.py
+++ primeiro-codigo.py
@@ -1,1 +1,15 @@
 print("Estou aprendendo programação e GitHub!")
+
+Codigo de Portugol Webstudio onde ser uma Calcularadora que pode ser mudada com alteraçao para Adiçao, Subtraçao, Divisao, Multiplcaçao
+programa {
+ funcao inicio() {
+real num1, num2, multiplicacao , resultado
+escreva("informe o primeiro valor numérico: ")
+leia(num1)
+escreva("informe o segundo valor numérico: ")
+leia(num2)
+multiplicacao=num1*num2
+resultado=num1*num2
+escreva("O resultado da multiplicaçao numérica dada é igual a ", resultado)
+ }
+}
